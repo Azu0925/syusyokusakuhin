f6119e9d732eac821c837b57bb7538f08f6676e3
Azu0925 <yamaneko564783@gmail.com>
2021-11-06 15:33:17 +0900

    react-router-dom

---
+++ horizon-web/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = ():JSX.Element => {
+  return (
+    <>
+      <p>Hello World</p>
+    </>
+  )
+}
+
+export default Home
---
+++ horizon-web/src/pages/NotFound.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NotFound = ():JSX.Element => {
+  return (
+    <>
+      <header>
+
+      </header>
+      <main>
+        <p>Sorry, Not found page</p>
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default NotFound
